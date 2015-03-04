e0722fb59db3afb4a5c0a6ade53e2bbc17224875
Akshay <akshay.ohri@gmail.com>
2015-03-04 17:42:50 +0530

    Standalone rep for pylibmc for Python3

---
+++ setup.py
@@ -0,0 +1,48 @@
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+import django_pylibmc
+
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='django-pylibmc',
+    version=django_pylibmc.__version__,
+    description='Django cache backend using pylibmc',
+    long_description=open('README.rst').read(),
+    author='Jeff Balogh',
+    author_email='jbalogh@mozilla.com',
+    url='http://github.com/jbalogh/django-pylibmc',
+    license='BSD',
+    packages=['django_pylibmc'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['pylibmc>=1.1', 'Django>=1.2'],
+    tests_require=['tox'],
+    cmdclass = {'test': Tox},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        # I don't know what exactly this means, but why not?
+        'Environment :: Web Environment :: Mozilla',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
---
+++ tests/settings.py
@@ -0,0 +1,31 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'testing',
+        'SUPPORTS_TRANSACTIONS': False,
+    },
+}
+
+INSTALLED_APPS = (
+    'app',
+)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+        'LOCATION': 'localhost:11211',
+        'TIMEOUT': 500,
+        'BINARY': True,
+        'USERNAME': 'test_username',
+        'PASSWORD': 'test_password',
+        'OPTIONS': {
+            'tcp_nodelay': True,
+            'ketama': True
+        }
+    }
+}
+
+PYLIBMC_MIN_COMPRESS_LEN = 150 * 1024
+PYLIBMC_COMPRESS_LEVEL = 1  # zlib.Z_BEST_SPEED
+
+SECRET_KEY = 'secret'
